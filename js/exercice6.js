3205289346dbf2eddd670a20e534716d8c87a456
BattlePoap <alapetite.mathieu@gmail.com>
2018-02-26 16:59:20 +0100

    exo 7 : js / html

--- js/exercice6.js
+++ js/exercice6.js
@@ -28,14 +28,49 @@ class Client extends Personne {
     constructor(numClient,nom,prenom,age){
         super(nom,prenom,age); // appelle le constructeur de la classe mère
         this.numClient = numClient;
+        this.tabCommandes = [];
     }
 
     get identite(){
         return super.identite + " " + this.numClient;
     }
+
+    get ca() {
+        let ca = 0;
+        this.tabCommandes.forEach(commande => {
+            ca += commande.valeur;
+        });
+        return ca
+    }
+
+    ajoutCommande (commande){
+        this.tabCommandes.push(commande);
+    }
+
+
 }
 
 let c = new Client("1548625", "doe", "john", 99);
 console.log(c.prenom);
 console.log(c.identite);
 
+/*
+ * com = new Commande (1, "crayon", 5);
+ * com2 = new Commande (2, "papier", 10);
+ * // ...
+ * console.log(c.ca);
+ */
+
+class Commande {
+    constructor (numOrder,produit,valeur){
+        this.numOrder = numOrder;
+        this.produit = produit;
+        this.valeur = valeur;
+    }
+}
+
+
+c.ajoutCommande(new Commande (1, "crayon", 5));
+c.ajoutCommande(new Commande (2, "papier", 10));
+
+console.log(c.ca);
