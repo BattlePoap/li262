995016b1c39faec8606ae5abf79ed2304ed549c3
BattlePoap <alapetite.mathieu@gmail.com>
2018-02-26 15:05:34 +0100

    exo 4 : extraction d'objets

---
+++ js/exercice4.js
@@ -0,0 +1,19 @@
+// exctraction au format objet des champs id et nom
+function extractObject(utilisateur){
+    return {"id": utilisateur.id, "nom": utilisateur.nom};
+}
+
+// exctraction au format objet des champs id et nom
+function extractObject(utilisateur){
+    return [utilisateur.id, utilisateur.nom];
+}
+
+
+let user = {"id": 1, "nom": "alapetite", "prenom":"mathieu"};
+
+let {id, nom} = extractObject(user); // même champs que dans le return
+//let [id, nom] = extractTab(user); // ordre qui compte
+console.log(id);
+console.log(nom);
+
+
