81459811f926712ae7721dddfc237108a91d383b
BattlePoap <alapetite.mathieu@gmail.com>
2018-02-26 14:22:04 +0100

    exo 2 : fonctions

---
+++ js/exercice2.js
@@ -0,0 +1,60 @@
+function bonjour(){
+    console.log("bonjour");
+}
+
+bonjour();
+
+function affSomme(a,b){
+    console.log(a+b);
+}
+
+affSomme(2,3);
+affSomme(2,"3");
+
+function somme (a, b){
+    return a+b;
+}
+
+console.log(somme(2,3));
+
+let sommeVar = function (a,b) {
+    return a+b;
+};
+
+console.log(sommeVar(2,3));
+
+let longueFonction = function(maFunc){
+    console.log("je lance la longue function : " + maFunc.name);
+    setTimeout(() => {
+        maFunc(); // quelle fonction lancer quand temps écoulé
+    }, 3000); // temps
+    console.log("c'est lancé"); 
+};
+
+longueFonction(bonjour);
+
+let disBonjour = function (){
+    console.log("Je dis Bonjour");    
+};
+
+let client = {};
+client.prenom = "mathieu";
+client.nom = "alapetite";
+client.disBonjour = disBonjour;
+client.disBonjour();
+
+
+client.commandes = [123, 456, 222];
+client.getNbCommandes = function(){
+    return this.commandes.length;
+};
+
+client.ajoutCommande = function (nouvelleCommande){
+    this.commandes.push(nouvelleCommande);
+}
+
+console.log(client.prenom + " a commandé " + client.getNbCommandes() + " fois");
+client.ajoutCommande("789");
+console.log(client.prenom + " a commandé " + client.getNbCommandes() + " fois");
+
+
