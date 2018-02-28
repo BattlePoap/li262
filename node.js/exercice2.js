5fb55f0a9b9229ef567300f5af5c126f5c279d3b
BattlePoap <alapetite.mathieu@gmail.com>
2018-02-28 12:19:31 +0100

    Node.js : modules

--- node.js/exercice2.js
+++ node.js/exercice2.js
@@ -1,10 +1,13 @@
 // charger le module HTTP
 const http = require('http');
 
+// charger le module module1.js
+const module1 = require("./modules/module1");
+
 http.createServer(function (request, response) {
     console.log("requête reçue");
     response.writeHead(200, {"Content-Type": "text/html"});
-    //response.end("bonjour");
+    // response.end("bonjour");
     response.write("bonjour</br>");
     response.write("c'est du node.js</br>");
     response.end("au revoir");
